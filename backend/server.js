8ff5f025b8e70ba7d6b7c0bc02849f0e0f1ba2a5
Manikaaggarwal24 <angel24592@gmail.com>
2023-10-07 21:24:56 -0400

    #

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import  express  from "express";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
+
+connectDB(); //connect to mongodb
 
 const app = express();
 
@@ -10,13 +14,9 @@ app.get('/' , (req , res) => {
     res.send('apii runnung');
 });
 
-app.get('/api/products' , (req , res) => {
-    res.json(products);
-});
 
-app.get('/api/products/:id' , (req , res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products',productRoutes);
+
+
 
 app.listen(port, () => console.log (`server runnong on port ${port}`))
